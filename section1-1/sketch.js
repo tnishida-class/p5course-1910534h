6dae4ea80beb83fa51c880360d511970e56f7fa5
1910534h <angel.gonzalez.jp@gmail.com>
2021-10-26 16:19:57 +0900

    Update sketch.js

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -6,11 +6,11 @@ function draw() {
   background(0);
   strokeWeight(4);
   fill(178, 100, 162);
-  triangle(12,120,116,120,116,10);
+  triangle(17,120,116,120,116,15);
   fill(94, 185, 84);
   triangle(0,108,106,108,106,0);
-  fill(255);
-  textSize(32);
+  fill(240);
+  textSize(45);
   textFont("serif");
-  text("46", 68, 100);
+  text("My Life is a Lie", 68, 100);
 }
